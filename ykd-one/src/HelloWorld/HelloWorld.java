a7f936fc11b745ab2eb5fdda860b888755479945
jjyjj <3526700180@qq.com>
2019-06-03 09:35:55 +0800

    Initial commit

---
+++ ykd-one/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+/**
+ * @author jjy
+ * @date 2019/6/3 - 8:32
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world ");
+    }
+
+}
